82115be3708253a3bd5c12bcc4815af4b3b659a6
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-17 12:21:16 -0700

    Add Register component with grid from semantic-ui

---
+++ src/components/Auth/Register.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+class Register extends React.Component {
+  state = {}
+
+  handleChange = () => {}
+
+  render() {
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        
+      </Grid>
+    )
+  }
+}
+
+export default Register;
