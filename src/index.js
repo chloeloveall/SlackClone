bb77b8335f38bfff0bd22240460c26dd793663c2
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-17 12:28:57 -0700

    Add routes, import components, import react-router-dom, import semantic ui css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
+    </Switch>
+  </Router>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
