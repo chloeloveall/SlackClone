f854b7de1a6cadcdebe5784a1c57349c6edbb8a1
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-18 16:06:37 -0700

    Add Channels component, import semantic-ui, add child menu

---
+++ src/components/SidePanel/Channels.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+// stateful component
+class Channels extends React.Component {
+  render() {
+    return (
+      <Menu.Menu>
+        
+      </Menu.Menu>
+    );
+  }
+}
+
+export default Channels;
