f7b8d37209e0816a0fdc91f3506108abff5386f6
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-17 17:06:46 -0700

    add authstate listener for login/register, add withRouter

--- src/index.js
+++ src/index.js
@@ -3,22 +3,37 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
+import firebase from './firebase';
 import 'semantic-ui-css/semantic.min.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
-    </Switch>
-  </Router>
-);
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    );
+  }
+} 
+
+const RootWithAuth = withRouter(Root);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <Router>
+      <RootWithAuth />
+    </Router>,
   </React.StrictMode>,
   document.getElementById('root')
 );
