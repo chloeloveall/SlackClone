4b2b1a6f129de1fda28823337984a881be598247
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-05 13:55:02 -0700

    Create isImage function to check if message is image, use ternanry to post image or message

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Comment } from 'semantic-ui-react';
 import moment from 'moment';
+import { Comment, Image } from 'semantic-ui-react';
 
 // adds special class to message if written by current user
 const isOwnMessage = (message, user) => {
   return message.user.id === user.uid ? 'message__self' : '';
-}
+};
+
+const isImage = message => {
+  return message.hasOwnProperty('image') && !message.hasOwnProperty('content');
+};
 
 const timeFromNow = timestamp => moment(timestamp).fromNow();
 
@@ -17,9 +21,9 @@ const Message = ({ message, user }) => (
       <Comment.Metadata> 
         {timeFromNow(message.timestamp)} 
       </Comment.Metadata>
-      <Comment.Text>
-        {message.content}
-      </Comment.Text>
+      {isImage(message) 
+        ? <Image src={message.image} className='message__image'/> 
+        : <Comment.Text>{message.content}</Comment.Text>}
     </Comment.Content>
   </Comment>
 );
